022ee5d1d25009bc196ff9677d9049dbafa3224d
TamerJr <ahmttmer@gmail.com>
2023-04-10 03:37:58 +0200

    done value section

--- zulu/src/Components/Values/Values.jsx
+++ zulu/src/Components/Values/Values.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import "./Values.css"
+import React from "react";
+import "./Values.css";
+import imgsec from "../../assets/imgforth.jpg";
 const Values = () => {
   return (
-    <section className='ValuesSection'>
-        <h2>
-    
-        </h2>
-        <img />
+    <section className="ValuesSection">
+      <h1>About Us</h1>
+      <div className="AboutUsDiv">
+        <div className="TextDesc">
+          <h2>Long Trip , Less Cost</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptas soluta placeat necessitatibus ad totam quam voluptate, debitis magnam ab iure earum cum, cupiditate itaque corporis officiis a saepe vitae dicta sint quaerat esse excepturi accusantium. Aliquam doloribus numquam vitae?
+          </p>
+            <button className="ctaAboutUs"> Know More</button>
+        </div>
+        <div className="ImgContainer">
+          <img src={imgsec} />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Values
+export default Values;
